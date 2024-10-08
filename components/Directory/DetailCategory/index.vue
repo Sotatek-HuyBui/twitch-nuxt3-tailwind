b2cf26f900c34bba8e52d2bc87e098f8bb21d1bc
buiquanghuy742 <buiquanghuy742@gmail.com>
2024-10-08 08:08:47 +0000

    Remove log

--- components/Directory/DetailCategory/index.vue
+++ components/Directory/DetailCategory/index.vue
@@ -73,10 +73,6 @@ const route = useRoute();
 const targetCategory = dummyCategories.find(
     (item) => item.name === route.params.live.split('-').join(' ')
 );
-console.log(
-    "🚀 ~ route.params.live.split('-').join(' '):",
-    route.params.live.split('-').join(' ')
-);
 
 const onFollow = () => {
     message.info('Function is in progress!');
